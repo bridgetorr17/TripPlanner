4c1c1d3ca8ce1bed6115019441d83294952d5f08
bridgetorr17 <bridgetorr1902@gmail.com>
2025-07-04 09:36:40 -0400

    working on method override

--- routes/trips.js
+++ routes/trips.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.get('/createNew', tripController.getCreateNewTrip);
 router.post('/createNew', tripController.postCreateNewTrip);
 router.get('/edit/aiSuggestion/:id', tripController.getSuggestion);
-router.delete('/delete', tripController.deleteTrip);
+router.delete('/delete/:id', tripController.deleteTrip);
 router.get('/:id', ensureAuth, tripController.getTrip);
 router.put('/edit/:id', tripController.updateTrip);
 
